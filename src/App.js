79aec06c56866a3d9f247aea54322a5a62d3c690
Ethan Pollack <epollack31@gmail.com>
2022-11-04 02:42:06 -0400

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo-bad.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          IQP: Nitrogen Cycle Public Outreach and Game Development
+        </p>
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
