45f8ae694e69080d8b61ab13e590a9beb3c03135
Ethan Pollack <epollack31@gmail.com>
2022-11-13 21:49:44 -0500

    Added Data Section

--- src/Cycle/Cycle.js
+++ src/Cycle/Cycle.js
@@ -1,29 +1,56 @@
-
+import React, { Component } from 'react';
 import './styles.css';
 
-function Cycle() {
-  return (
-    <div className="main">
-      <div className="items">
-        
+class Cycle extends React.Component {
+  
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hour:   0,
+      food:   0,
+      n2:     50,
+      runoff: 0 
+    }
+  };
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }; 
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);  
+  };
+
+  tick() {    
+    this.setState({      
+      hour: this.state.hour + 1  
+    });
+  };
+
+  render () {
+    return (
+      <div className="main">
+        <div className="items">
+          <h1> Items: </h1>
+          <div></div>
+        </div>
+        <div className="data">
+          <h1> Data: </h1>
+          <p>Food: { this.state.food } </p>  
+          <p>Hour: { this.state.hour } </p>  
+          <p>Nitrogen Content: { this.state.n2} </p>  
+          <p>Runoff: { this.state.runoff } </p>  
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  };
+
+}
+
 
 export default Cycle;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
