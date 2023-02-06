8203472a63c0763204bee846c0eb5f8d56a79398
phbui <101759075+phbui@users.noreply.github.com>
2023-02-06 16:05:01 -0500

    Added Tiles

--- src/Encyclopedia/Pages/EncyclopediaHome.js
+++ src/Encyclopedia/Pages/EncyclopediaHome.js
@@ -5,22 +5,23 @@ import '../styles.css';
 function EncyclopediaHome(props) {
 
     return (
-        <div className='main'>
-            <h1>Encyclopedia Home Page</h1>
-            <p>Welcome to the encyclopedia.</p>
-            <p>Here is a list of all the current pages:</p>
-            <ul>
-                <li>
-                    <span className="en-link" onClick={props.update("Potato Plant")}>Potato Plant</span>
-                </li>
-                <li>
-                    <span className="en-link" onClick={props.update("Food Silo")}>Food Silo</span>
-                </li>
-                <li>
-                    <span className="en-link" onClick={props.update("Ammonium Silo")}>Ammonium Silo</span>
-                </li>
-            </ul>
-        </div>
+            <div className='main'>
+                <h1>Encyclopedia Home Page</h1>
+                <p>Welcome to the encyclopedia.</p>
+                <p>Here is a list of all the current pages:</p>
+                    <div class='true-center'>
+                        <button class="butt" type="button" onClick={props.update("Potato Plant")}>
+                            <img src="https://i.ibb.co/CW5Wvry/buttonpng.png" alt="buttonpng" border="0"/>
+                        </button>
+                        <button class="butt" type="button" onClick={props.update("Food Silo")}>
+                            <img src="https://i.ibb.co/CW5Wvry/buttonpng.png" alt="buttonpng" border="0"/>
+                        </button>
+                        <button class="butt" type="button" onClick={props.update("Ammonium Silo")}>
+                            <img src="https://i.ibb.co/CW5Wvry/buttonpng.png" alt="buttonpng" border="0"/>
+                        </button>
+                    </div>
+                    
+            </div> 
     );
 }
 
