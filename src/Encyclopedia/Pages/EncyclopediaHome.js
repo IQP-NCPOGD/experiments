fc4a606e73209c7a962624998a81ebd23bdafb70
phbui <101759075+phbui@users.noreply.github.com>
2023-02-06 20:05:05 -0500

    Buttons

--- src/Encyclopedia/Pages/EncyclopediaHome.js
+++ src/Encyclopedia/Pages/EncyclopediaHome.js
@@ -11,12 +11,21 @@ function EncyclopediaHome(props) {
                 <p>Here is a list of all the current pages:</p>
                     <div class='true-center'>
                         <button class="butt" type="button" onClick={props.update("Potato Plant")}>
+                            <div class='banner'>
+                                <h4>Potato Plant</h4>
+                            </div> 
                             <img src="../../../../logo192.png" alt="buttonpng" border="0"/>
                         </button>
                         <button class="butt" type="button" onClick={props.update("Food Silo")}>
+                            <div class='banner'>
+                                <h4>Food Silo</h4>
+                            </div> 
                             <img src="../../../../logo192.png" alt="buttonpng" border="0"/>
                         </button>
                         <button class="butt" type="button" onClick={props.update("Ammonium Silo")}>
+                            <div class='banner'>
+                                <h4>Ammonium Silo</h4>
+                            </div> 
                             <img src="../../../../logo192.png" alt="buttonpng" border="0"/>
                         </button>
                     </div>
